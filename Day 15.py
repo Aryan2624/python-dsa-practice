fa93550bec992facb010d7f7589521288e11c214
Aryan Dubey <blackarch.2426@gmail.com>
2026-09-26 23:23:40 +0530

    Create Day 15.py
    it was day 15  for my practice for python dsa code

---
+++ Day 15.py
@@ -0,0 +1,22 @@
+#Valid Palindrome
+
+def is_palindrome(s):
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+
+        if s[left] != s[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+
+s = "madam"
+
+result = is_palindrome(s)
+
+print("Palindrome:", result)
